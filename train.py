4b82342149236cf2f3ded6688a934f53c92b01f5
RaikohGrass <83981857+RaikohGrass@users.noreply.github.com>
2022-01-27 17:23:16 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -9,46 +9,32 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from sklearn.tree import DecisionTreeRegressor
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
-path ="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+path ="https://raw.githubusercontent.com/RaikohGrass/ML_Azure_Capstone/main/precipitation_LA.csv"
 
 ds = TabularDatasetFactory.from_delimited_files(path)
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    
+    
+    df = ds.to_pandas_dataframe()
+    # We drop the empty column TAVG
+    df = df.drop('TAVG')
+    # Since we are not using deep learning or techniques that have a notion of time we can drop the date and PGTM columns
+    # We also drop the station name column
+    df = df.drop(['STATION','NAME','DATE','PGTM'])
+    x = df
+    y = df.pop('PRCP')
 
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_train,x_test,y_train,y_test
 
-    return x_df,y_df
+x_train,x_test,y_train,y_test = clean_data(ds)
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
 run = Run.get_context()
 
@@ -56,15 +42,23 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--criterion', type=str, default='squared_error', help="Function to measure the quality of a split")
+    parser.add_argument('--max_depth', type=int, default=None, help="Maximum depth for the trees")
+    parser.add_argument('--min_samples_split', type=int, default=2, help="Minimum number of samples needed for a split")
+    parser.add_argument('--max_leaf_nodes', type=int, default=None, help="Maximum number of nodes")
+
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Split Criterion:", np.str(args.criterion))
+    run.log("Max depth:", np.int(args.max_depth))
+    run.log("Min samples split:", np.int(args.min_samples_split))
+    run.log("Max leaf nodes:", np.int(args.max_leaf_nodes))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = LogisticRegression(criterion = args.criterion,
+                               max_depth = args.max_depth,
+                               min_samples_split = args.min_samples_split,
+                               max_leaf_nodes = args.max_leaf_nodes).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
